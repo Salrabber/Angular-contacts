6cedcdf30f18a0af44f656b4bb544a8969abb7dc
Salrabber <salrabber@gmail.com>
2022-09-22 19:21:09 +0300

    user-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
